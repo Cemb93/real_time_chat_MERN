99fefda6b36887deef4d1e59b9419a905840b6b3
Cemb93 <karloz13m@gmail.com>
2024-05-01 12:36:57 -0500

    login user successfull

--- api/src/routes/users.routes.ts
+++ api/src/routes/users.routes.ts
@@ -1,8 +1,11 @@
 import { Router } from "express";
 import { register } from "../services/users/register";
+import { login } from "../services/users/login";
 
 export const usersRouter = Router();
 
 const REGISTER = "register";
+const LOGIN = "login";
 
 usersRouter.post(`/${REGISTER}`, register);
+usersRouter.post(`/${LOGIN}`, login);
---
+++ api/src/services/users/login.ts
@@ -0,0 +1,50 @@
+import { Request, Response } from "express";
+import { IUsers } from "../../interface/IUsers";
+import { usersModel } from "../../models/Users";
+import { passwordCorrecto } from "../../tools/bcrypt";
+import { generarToken } from "../../tools/jwToken";
+
+export async function login(req: Request, res: Response) {
+  const user = req.body as IUsers;
+
+  try {
+    const existUser: IUsers | null = await usersModel.findOne({
+      email: user.email,
+    });
+
+    if (!existUser) {
+      return res.status(401).json({
+        error: "Esta cuenta no esta registrada",
+      });
+    }
+
+    if (!user.email || !user.password) {
+      return res.status(400).json({
+        error: "Todos los campos son requeridos",
+      });
+    }
+
+    const passwordEncriptado = existUser.password;
+
+    const compararPassword = await passwordCorrecto(
+      user.password,
+      passwordEncriptado
+    );
+
+    if (compararPassword) {
+      const token = await generarToken(existUser.email);
+
+      const data = {
+        user: existUser,
+        token,
+      };
+
+      return res.status(200).json({ msg: "Session y token valido", data });
+    }
+  } catch (error) {
+    if (error instanceof Error) {
+      return res.status(400).json({ error: error.message });
+    }
+    console.log("Error en register por:", error);
+  }
+}
